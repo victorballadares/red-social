8dfd805438c3b54c761c95d6db65ae402cd96bf0
victorballadares <victor.m.balladares.f@gmail.com>
2022-05-27 14:03:05 +0200

    Rename some buttons

--- routes/main.js
+++ routes/main.js
@@ -3,8 +3,17 @@ var router = express.Router();
 const auth = require('../config/auth');
 
 /* GET home page. */
-router.get('/', auth, function(req, res, next) {
-    console.log(req.user)
+router.get('/', auth, (req, res, next) => {
+    res.render('main');
+});
+
+router.get('/post', auth, (req, res, next) => {
+    res.render('publish', {
+        error: req.flash("error"),
+    });
+});
+
+router.post('/post', auth, (req, res, next) => {
     res.render('main');
 });
 
